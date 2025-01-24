727e82b9a628f6e1c282614377135932fde91102
TaeyoonGo <taeyoongo007@gmail.com>
2025-01-24 15:28:35 +0900

    chore : 포트폴리오 환경 세팅 완료
    변경 :
     ✔ gulp 환경 세팅
     ✔ ScrollTrigger + ScrollSmooth Proxy
     ✔ markers 커스텀 완료
    
     [skip-ci]

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import {markers} from "./markers.js";
+import {settings} from "./settings.js";
+
+document.addEventListener('DOMContentLoaded',()=>{
+    markers();
+    settings();
+})
+
+
---
+++ src/js/markers.js
@@ -0,0 +1,10 @@
+const markers = ()=>{
+    if (document.querySelector('.gsap-marker-scroller-start')) {
+        const markers = gsap.utils.toArray('[class *= "gsap-marker"]');
+        scrollbar.addListener(({ offset }) => {
+            gsap.set(markers, { marginTop: -offset.y });
+        });
+    }
+}
+
+export {markers}
