c87c9b6a1d6b452352457dd7ed849a5f1344e5c6
TaeyoonGo <taeyoongo007@gmail.com>
2025-06-29 18:17:35 +0900

    MouseEvent 적용 완료

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@ import {setLayout} from "./settings.js";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText, ScrollToPlugin);
 
-
 const tl = gsap.timeline();
 const mm = gsap.matchMedia();
 
@@ -19,13 +18,14 @@ let smoother = ScrollSmoother.create({
     normalizeScroll: true
 });
 
+
 const IntroProgress = () => {
     const items = document.querySelectorAll('.splash-screen .item')
     const splashItems = gsap.utils.toArray(items)
     gsap.set(splashItems, {yPercent: 0})
     const animation = gsap.timeline()
     animation
-        .to('.progress-box',{autoAlpha:0,duration:0.3})
+        .to('.progress-box', {autoAlpha: 0, duration: 0.3})
         .to(splashItems, {
             yPercent: 100,
             duration: 1,
@@ -35,7 +35,7 @@ const IntroProgress = () => {
                 from: 'center'
             },
         }, '+=0.5')
-        .to('.splash-loader-box',{visibility:'hidden'})
+        .to('.splash-loader-box', {visibility: 'hidden'})
     return animation
 }
 const IntroText = () => {
@@ -52,8 +52,8 @@ const IntroText = () => {
         }
     })
     const animation = gsap.timeline()
-    animation.textEffect('.section01 h3:first-child')
-    animation.textEffect('.section01 h3:last-child', "<")
+    animation.textEffect('.section1 h3:first-child')
+    animation.textEffect('.section1 h3:last-child', "<")
 
     return animation
 }
@@ -84,14 +84,40 @@ const IntroMasterTimeline = () => {
     MasterAnimation
         .add(IntroProgress())
         .add(IntroText(), "+=0.3")
-        .add(HeaderAni(), '+=1')
-        .add(ImgStarAni(), '<')
+        .add(ImgStarAni(), '+=1')
+        .add(HeaderAni(), '<')
+        // .add(circleAni())
+        .add(() => {
+            smoother.paused(false)
+        })
 }
 
-function init() {
-    // 1. 화면 초기 세팅 및 Gsap AutoAlpha 셋팅 ✅
-    // 2. Text & Lottie 0% ~ 100% Progress 진행 ✅
-    // 3. 다음 애니메이션으로 동작하기 위한 체크 진행 ✅
+const navigation = () => {
+    let navList = gsap.utils.toArray('.navi-list > li')
+    let header = document.querySelector('.sticky-header')
+
+
+    navList.forEach((item, index) => {
+        const tl = gsap.timeline({paused: true})
+            .to(item.querySelector('span'), {color: '#bbdde0'}, 0)
+            .to(item.querySelector('div'), {scaleX: 1, duration: 0.3}, 0)
+
+
+        item.addEventListener('mouseover', () => tl.play())
+        item.addEventListener('mouseleave', () => tl.reverse())
+        item.addEventListener('click',()=>{
+
+        })
+    })
+
+
+
+
+
+
+}
+
+const Loading = () => {
     const lottie = Lottie.loadAnimation({
         container: document.querySelector('#lottie'),
         loop: true,
@@ -99,7 +125,6 @@ function init() {
         path: '/lottie/loading.json'
     });
     const img = gsap.utils.toArray('img')
-
     const updateProgress = (instance) => {
         let numberPercent = instance.progressedCount / img.length
         const loaderText = document.querySelector('.loader-text')
@@ -108,81 +133,87 @@ function init() {
             lottie.pause()
         }
     }
-
-    imagesLoaded(img).on('progress', updateProgress)
+    imagesLoaded(img)
+        .on('progress', updateProgress)
         .on('always', IntroMasterTimeline)
 }
 
+const mousePoint = () =>{
+    let cursor = document.querySelector('#cursor');
+    let body = document.querySelector('body')
 
-function scrollImg() {
+
+    // console.log(cursor, body)
+    // let widthToProgress = gsap.utils.mapRange(0,innerWidth,0,100);
+
+    let xTo = gsap.quickTo(cursor,"x",{duration:0.4,ease:'power3'})
+    let yTo = gsap.quickTo(cursor,"y",{duration:0.4,ease:'power3'})
+
+    body.addEventListener('mousemove',({clientX : x ,clientY: y})=>{
+            xTo(x - (cursor.offsetWidth * 0.5))
+            yTo(y - (cursor.offsetHeight * 0.5))
+        console.log(x,y)
+    })
+
+
+}
+
+const circleAni = () => {}
+
+
+
+
+function init() {
+    smoother.paused(true);
+    document.scrollingElement.scrollTo(0, 0);
+    Loading();
+
+    mousePoint();
+    navigation();
+
+    homeAni();
+    abilityAni();
+    workAni();
+    contactAni();
+}
+
+
+const homeAni = () => {
     const nav = document.querySelector('.sticky-header')
 
     const timeline = gsap.timeline()
-        .to('.img-star',{
-            rotation:360,
-            scale:50,
+        .to('.img-star', {
+            rotation: 360,
+            scale: 100,
         })
     ScrollTrigger.create({
-        trigger:'.section01',
-        start:'top top',
-        end:innerHeight + nav.offsetHeight,
-        scrub:true,
-        pin:true,
-        pinSpacing:true,
-        animation:timeline,
+        trigger: '.section1',
+        start: 'top top',
+        pin: true,
+        pinSpacing: true,
+        scrub: true,
+        markers: true,
+        animation: timeline,
     })
 }
 
-
-function horizontalTextScrolling() {
-    const {words} = new SplitText('.section03 h3', {type: 'words'})
-
-
-
-    ScrollTrigger.create({
-        trigger:'.section03',
-        start:'top top',
-        end:'+=3000',
-        animation: gsap.to(words,{x:-innerWidth}),
-        pin:true,
-        pinSpacing:true,
-        // markers:true,
-        scrub:true,
-    })
+const abilityAni = () => {
+    const {words} = new SplitText('.section3 .skill-list', {type: 'words'})
 
 }
 
-horizontalTextScrolling()
-const navigation = () => {
-    let navList = gsap.utils.toArray('.navi-list > li')
-    let nav = document.querySelector('.sticky-header')
+const workAni = () => {
 
-    gsap.utils.toArray('.section').forEach((section, index) => {
-        // ScrollTrigger.create({
-        //     trigger:section,
-        //     start:()=> `top ${nav.offsetHeight}px`,
-        //     end:()=> `bottom ${nav.offsetHeight}px`,
-        //     toggleActions : 'restart none none reverse'
-        // })
-    })
+}
+const contactAni = () => {
 
-    navList.forEach((item, index) => {
-        const tl = gsap.timeline({paused: true})
-            .to(item.querySelector('span'), {color: '#bbdde0'}, 0)
-            .to(item.querySelector('div'), {scaleX: 1, duration: 0.3}, 0)
-
-        item.addEventListener('mouseover', () => tl.play())
-        item.addEventListener('mouseleave', () => tl.reverse())
-
-    })
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
     setLayout();
     init();
-    // navigation();
-    scrollImg();
+
 })
 
 
